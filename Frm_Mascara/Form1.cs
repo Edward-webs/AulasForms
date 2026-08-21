eb0abf09868aca562bb1d65c4ecc92b2c7364022
Edward-webs <edulyra.silva@gmail.com>
2026-08-21 11:36:28 -0300

    Valida_Mascara e Valida_CPF

---
+++ Frm_Mascara/Form1.cs
@@ -0,0 +1,96 @@
+namespace Frm_Mascara
+{
+    public partial class Form1 : Form
+    {
+        bool VerConteudotxt = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Hora_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "00:00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_VerConteudo_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = Msk_TextBox.Text;
+            if (VerConteudotxt == false)
+            {
+                Msk_TextBox.PasswordChar = '\0';
+                VerConteudotxt = true;
+            }
+            else
+            {
+                Msk_TextBox.PasswordChar = '*';
+                VerConteudotxt = false;
+            }
+        }
+
+        private void Btn_CEP_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "00000-000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Moeda_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "$ 000,000,000.00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Data_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "00/00/0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Telefone_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "(00) 0000-0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Msk_TextBox_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Senha_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Frm_ValidaCPF/Form1.cs
@@ -0,0 +1,38 @@
+namespace Frm_ValidaCPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Lbl_Resultado.Text = "";
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            bool validaCPF = false;
+            ValidaCpf valida = new ValidaCpf();
+            validaCPF = valida.Valida(Msk_CPF.Text);
+            if (validaCPF == true)
+            {
+                Lbl_Resultado.Text = "CPF VÁLIDO";
+                Lbl_Resultado.ForeColor = Color.Green;
+            }
+            else
+            {
+                Lbl_Resultado.Text = "CPF INVÁLIDO";
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+        }
+    }
+}
